43a27135c070fc6130883b58b652ac8fcbbb6216
Adiwoolwich <adiwolf86@gmail.com>
2026-08-29 21:00:10 +0200

    Restore App.tsx with swipeable sheet handle

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,19 @@
-PLACEHOLDER_APP
+import { useEffect, useMemo, useRef, useState } from "react";
+import { MapHost } from "./components/map-host";
+import {
+  LocateButton,
+  SearchAndFilters,
+  SearchBar,
+  StationPanel,
+} from "./components/station-panel";
+import { SiteFooter } from "./components/site-footer";
+import { DatenschutzPage, ImpressumPage } from "./components/legal-pages";
+import { FeedbackPage } from "./components/feedback-form";
+import { hasPreciseCoords, STATIONS } from "./lib/stations";
+import { allStations, applyFilters, useAppStore } from "./lib/store";
+import { inBounds } from "./lib/geo";
+import { hasMapDeepLink, parseUrl } from "./lib/url-state";
+import { t, useLang } from "./lib/i18n";
+import { cn } from "./lib/utils";
+
+const SKIP_KEY = "bl-skip-landing";
