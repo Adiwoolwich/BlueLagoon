e1baf94f48a04072763f8af66814e77f4b5e0915
Adiwoolwich <adiwolf86@gmail.com>
2026-08-29 21:00:42 +0200

    Restore hybrid map, Carto labels, and sheet swipe

--- src/App.tsx
+++ src/App.tsx
@@ -17,3 +17,27 @@ import { t, useLang } from "./lib/i18n";
 import { cn } from "./lib/utils";
 
 const SKIP_KEY = "bl-skip-landing";
+
+function useFilteredStations() {
+  const query = useAppStore((s) => s.query);
+  const filters = useAppStore((s) => s.filters);
+  const reports = useAppStore((s) => s.reports);
+  const userPos = useAppStore((s) => s.userPos);
+  const route = useAppStore((s) => s.route);
+  const routePath = useAppStore((s) => s.routePath);
+  const corridorKm = useAppStore((s) => s.corridorKm);
+  const extraStations = useAppStore((s) => s.extraStations);
+  return useMemo(
+    () =>
+      applyFilters(allStations(extraStations), {
+        query,
+        filters,
+        reports,
+        userPos,
+        route,
+        routePath,
+        corridorKm,
+      }),
+    [query, filters, reports, userPos, route, routePath, corridorKm, extraStations],
+  );
+}
